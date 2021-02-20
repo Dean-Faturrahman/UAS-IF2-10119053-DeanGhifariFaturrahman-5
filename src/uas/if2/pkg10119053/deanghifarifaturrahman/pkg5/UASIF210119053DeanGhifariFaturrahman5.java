3dda02dc6264913ac076b6993f0197af63c971f9
Dean <Dean@DESKTOP-FV1SF0U>
2021-02-20 16:09:07 +0700

    Merge origin/master
    Conflicts:
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml
    	src/project/lavanderia/controller/AdminController.java
    	src/project/lavanderia/controller/PelangganController.java
    	src/project/lavanderia/impl/PelangganDaoImpl.java
    	src/project/lavanderia/model/PelangganModel.java
    	src/project/lavanderia/model/TabelPelangganModel.java
    	src/project/lavanderia/model/TabelPesananModel.java
    	src/project/lavanderia/view/AmbilPesananView.form
    	src/project/lavanderia/view/AmbilPesananView.java
    	src/project/lavanderia/view/DataTerambilView.java
    	src/project/lavanderia/view/TambahAntrianView.java

---
+++ src/uas/if2/pkg10119053/deanghifarifaturrahman/pkg5/UASIF210119053DeanGhifariFaturrahman5.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uas.if2.pkg10119053.deanghifarifaturrahman.pkg5;
+
+/**
+ *
+ * @author Dean
+ * UAS PBO
+ * NIM     : 10119053
+ * NAMA    : Dean Ghifari Faturrahman
+ * Kelas   : IF - 2
+ * No Soal : 5
+ */
+public class UASIF210119053DeanGhifariFaturrahman5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
